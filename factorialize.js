661f9904c8062bc1502c5de4e73e77e69b085917
donnywarren <51206190+donnywarren@users.noreply.github.com>
2020-03-19 20:30:20 -0400

    finished

--- factorialize.js
+++ factorialize.js
@@ -1,7 +1,11 @@
 //FACTORIALIZE
 
 const factorialize = (num) => {
-
+  var result = 1;
+  for (var i = 2; i <= num; i++) {
+    result *= i;
+  }
+  return result;
 }
 
 console.log(factorialize(4))
